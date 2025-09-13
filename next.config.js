d503f53e109b8ddb12ca99026175587c1b57833f
Ankithareddy08 <ankithareddy0804@gmail.com>
2025-09-13 20:49:07 +0530

    Fix: Revert to standard next build script

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // IMPORTANT: Replace 'Ankitha-reddy---portfolio' with your exact repository name.
+  basePath: '/Ankitha-reddy---portfolio',
   images: {
     unoptimized: true,
   },
